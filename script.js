1a52a38347d7d2da0accd5699f2435758ba6cdca
gudu0 <125727026+Gudu0@users.noreply.github.com>
2024-10-14 12:27:54 -0700

    Update script.js
    Made all fone open sans hopefully at button press?

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function frank(){
+  e.classList.add("open-sans");
   e.classList.toggle("handlee-regular");
   heading.classList.remove("handlee-regular");
-  heading.classList.add("open-sans");
+  //heading.classList.add("open-sans");
 }
 test.onclick = frank;
