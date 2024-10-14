375f4e6c9d81cd95c0552d7caa6216a347cdb504
gudu0 <125727026+Gudu0@users.noreply.github.com>
2024-10-14 12:19:18 -0700

    Update script.js
    maybe fixed issue, heading please be open sans :)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function frank(){
   e.classList.toggle("handlee-regular");
   heading.classList.remove("handlee-regular");
-//  heading.classList.add("open-sans");
+  heading.classList.add("open-sans");
 }
 test.onclick = frank;
